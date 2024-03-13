954a1707d12d6ce5e508bfad2f44babe9fd631d3
Dev Italiya <devitaliya22@gmail.com>
2024-03-13 19:40:42 +0530

    Added more things

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,19 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
 function App() {
   const [message, setMessage] = useState("");
   const [socket, setSocket] = useState(null);
   const [messages, setMessages] = useState([]);
+  const [room, setRoom] = useState("");
+  const [socketId, setSocketId] = useState("");
+  const [roomJoined,setRoomJoined] = useState(false)
 
   useEffect(() => {
     const socket = io("http://localhost:3000");
+    socket.on("connect", () => {
+      setSocketId(socket.id);
+    });
     setSocket(socket);
 
     return () => {
@@ -23,16 +29,48 @@ function App() {
     }
   }, [socket]);
 
+  useEffect(() => {
+    if (socket) {
+      socket.on("send-to-room", (data) => {
+        setMessages((prev) => [...prev, data + " ---- In my personal room ---- "]);
+      });
+    }
+  }, [socket]);
+
   const handleSendMessage = () => {
-    socket.emit("send-to-my-server", message);
-    setMessage("");
+    if (message && socket) {
+      socket.emit("send-to-my-server", message, room);
+      setMessage("");
+      // setRoom("");
+    }
+  };
+
+  const handleJoinRoom = () =>{
+    if(room  && socket)
+    {
+      socket.emit("join-room",room)
+      setRoomJoined(true);
+      // setRoom("")
+    }
   }
 
   return (
     <div>
       <input type="text" value={message} onChange={(e) => setMessage(e.target.value)} />
+      <input type="text" value={room} onChange={(e) => setRoom(e.target.value)} />
+      <button onClick={handleJoinRoom}>Join room</button>
+
       <button onClick={handleSendMessage}>Send Message</button>
-      {messages.length > 0 ? <>{messages[messages.length - 1]}</> : <>empty</>}
+      {
+        roomJoined ? <div>Room joined</div> : <div>Room Not joined yet</div>
+      }
+      <div>
+        <br/>
+        <div>You are connected with : {socketId}</div>
+        {messages.map((message, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </div>
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+// server.js
 import express from "express";
 import cors from "cors";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const server = createServer(app);
@@ -13,6 +15,7 @@ const io = new Server(server, {
   }
 });
 
+app.use(cookieParser());
 app.use(
   cors({
     origin: "*",
@@ -22,17 +25,37 @@ app.use(
 );
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Connected and live");
 });
 
 io.on("connection", (socket) => {
   console.log("User connected: ", socket.id);
 
-  socket.on("send-to-my-server", (data) => {
-    io.emit("send-to-all-clients", data);
+  socket.on("send-to-my-server", (message, room) => {
+    console.log(message);
+    if (room === '') {
+      // io.emit("send-to-all-clients", message); if u wanna send to everyone including yourself
+      
+      //if u wanna send to all othere except yourself
+      socket.broadcast.emit("send-to-all-clients", message);
+    } else {
+      io.to(room).emit("send-to-room", message);//this means message will go to all users including u who has joined the room
+
+      // socket.to(room).emit("send-to-room", message);
+      //means message will only go to all people joined room but not to you
+    }
   });
+
+
+  socket.on("join-room",(room)=>{
+    console.log(`Room joined by ${socket.id} and ${room}`);
+    socket.join(room)
+  })
+
 });
 
 server.listen(3000, () => {
   console.log("Listening on port 3000");
 });
+
+export { app, server };
